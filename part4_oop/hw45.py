7a38e31ddb2cac7c3003ebb009507eb28b84b503
Вероника <ivanova.ve@phystech.edu>
2026-03-31 23:18:22 +0300

    fix

--- part4_oop/hw45.py
+++ part4_oop/hw45.py
@@ -150,9 +150,6 @@ class CachedProperty[V]:
         self.func_name = func.__name__
 
     def __get__(self, instance: HasCache[Any, Any] | None, owner: type) -> V:
-        if instance is None:
-            return self
-
         if instance.cache.exists(self.func_name):
             val_from_cache: V | None = instance.cache.get(self.func_name)
             if val_from_cache is not None:
